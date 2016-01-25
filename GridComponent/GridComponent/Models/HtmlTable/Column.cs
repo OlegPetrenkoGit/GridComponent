4f4c6ec4680b4c7dcc848e08174291e7c0d5e0cc
OlegPetrenko <mail.petrenko.oleg@gmail.com>
2016-01-25 17:39:43 +0200

    fix missing files issue

---
+++ GridComponent/GridComponent/Models/HtmlTable/Column.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace GridComponent.Models.HtmlTable
+{
+    public class Column
+    {
+        public string Name { get; set; }
+        public bool ReadOnly { get; set; }
+        public Type Type { get; set; }
+    }
+}
---
+++ GridComponent/GridComponent/Models/HtmlTable/Interface/IEntryCell.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace GridComponent.Models.HtmlTable.Interface
+{
+    public interface IEntryCell
+    {
+        string Name { get; }
+        object Value { get; }
+
+        TagBuilder ToHtmlTag();
+    }
+}
